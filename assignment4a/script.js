44e39eb2b37f4f66e0c78336ef426a8254bea47d
victornwabueze <victor.nwabuezeokpara@dcmail.ca>
2026-03-25 16:30:12 -0400

    Commit 4
    Completed JavaScript logic including the random story generation, user input replacements, and the UK conversion

--- assignment4a/script.js
+++ assignment4a/script.js
@@ -19,8 +19,10 @@ const events = [
   "turned into a slug and slithered away"
 ];
 function returnRandomStoryString() {
-  // It was 94 Fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
-
+const randomCharacter = randomValueFromArray(characters);
+const randomPlace = randomValueFromArray(places);
+const randomEvent = randomValueFromArray(events);
+let storyText = `It was 94 Fahrenheit outside, so ${randomCharacter} went for a walk. When they got to ${randomPlace}, they stared in horror for a few moments, then ${randomEvent}. Bob saw the whole thing, but was not surprised — ${randomCharacter} weighs 300 pounds, and it was a hot day.`;
   return storyText;
 }
 
@@ -29,16 +31,19 @@ function returnRandomStoryString() {
 generateBtn.addEventListener("click", generateStory);
 
 function generateStory() {
+    let newStory = returnRandomStoryString();
   if (customName.value !== "") {
     const name = customName.value;
+      newStory = newStory.replace("Bob", name);
   }
 
-  if (document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature = Math.round(94);
-  }
-
+if (document.getElementById("uk").checked) {
+  const weight = `${Math.round(300 / 14)} stone`;
+  const temperature = `${Math.round((94 - 32) * (5 / 9))} Celsius`;
+  newStory = newStory.replace("300 pounds", weight);
+  newStory = newStory.replace("94 Fahrenheit", temperature);
+}
   // TODO: replace "" with the correct expression
-  story.textContent = "";
+  story.textContent = "newstory";
   story.style.visibility = "visible";
 }
