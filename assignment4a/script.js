6ffa3c08c377e38c104f4d2ae8ce286079ceba90
victornwabueze <victor.nwabuezeokpara@dcmail.ca>
2026-03-25 15:14:08 -0400

    COMMIT 3
    Added arrays for characters, places, and events

--- assignment4a/script.js
+++ assignment4a/script.js
@@ -11,22 +11,13 @@ function randomValueFromArray(array) {
   return array[random];
 }
 
-// Raw text strings
-
-// Willy the Goblin
-// Big Daddy
-// Father Christmas
-
-// the soup kitchen
-// Disneyland
-// the White House
-
-// spontaneously combusted
-// melted into a puddle on the sidewalk
-// turned into a slug and slithered away
-
-// Partial return random string function
-
+const characters = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+const places = ["the soup kitchen", "Disneyland", "the White House"];
+const events = [
+  "spontaneously combusted",
+  "melted into a puddle on the sidewalk",
+  "turned into a slug and slithered away"
+];
 function returnRandomStoryString() {
   // It was 94 Fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
 
