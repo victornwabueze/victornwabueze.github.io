4b86416b381a78bfab3ed67b9deb63930fab1b36
victornwabueze <victor.nwabuezeokpara@dcmail.ca>
2026-03-27 11:40:57 -0400

    Commit 2
    I added structured comments to JavaScript and the required HTML file header comments

--- assignment4b/script.js
+++ assignment4b/script.js
@@ -1,5 +1,16 @@
+// Assignment 4b - Image gallery
+// Author: Victor Nwabueze
+// Description: Adding functionality to display images and interact with gallery
 const displayedImage = document.querySelector(".displayed-img");
 const thumbBar = document.querySelector(".thumb-bar");
 
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
+
+// Looping through images and creating thumbnails
+
+
+// Handling image click events
+
+
+// Darken/Lighten button functionality
