977ca053268f1919e45936ceb7a2f37a3feee636
victornwabueze <victor.nwabuezeokpara@dcmail.ca>
2026-03-27 12:12:58 -0400

    COMMIT 3
    I added the implementation of the thumbnail generation and click functionality for image gallery

--- assignment4b/script.js
+++ assignment4b/script.js
@@ -8,9 +8,20 @@ const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
 
 // Looping through images and creating thumbnails
-
+const imageFilenames = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
+for (let i = 0; i < imageFilenames.length; i++) {
+  const newImage = document.createElement("img");
+  newImage.setAttribute("src", `images/${imageFilenames[i]}`);
+  newImage.setAttribute("alt", `Thumbnail ${i + 1}`);
+  thumbBar.appendChild(newImage);
+}
 
 // Handling image click events
-
+thumbBar.addEventListener("click", function (e) {
+  if (e.target.tagName === "IMG") {
+    displayedImage.src = e.target.src;
+    displayedImage.alt = e.target.alt;
+  }
+});
 
 // Darken/Lighten button functionality
